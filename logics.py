0b89d40af542e5c908b66c4e6b1880ed932510c4
JohnKim27 <jungmyung27@gmail.com>
2019-05-14 16:34:54 +0300

    fixed money bar and added commandline output on arrow button and pause button working

--- logics.py
+++ logics.py
@@ -5,6 +5,15 @@
 import random
 import math
 
+class Elem:
+    dirt='Dirt'
+    evildirt='Evil Dirt'
+    empty='Empty'
+    fire='Fire'
+    heart='Heart'
+    chunk=['Chunk1','Chunk2','Chunk3']
+    player='player'
+SALARY={Elem.chunk[0]:100, Elem.chunk[1]:200, Elem.chunk[2]:300}
 
 class Player:
     def __init__(self, game, y, x):
@@ -25,3 +34,9 @@ class Player:
             self.game.g[y][x] = Elem.empty
         elif self.game.g[y][x] == Elem.evildirt:
             self.game.new_fire(y, x)
+
+class Command:
+    left='Moving Left'
+    right='Moving Right'
+    next='Digging Next'
+    down='Digging Down'
--- main.py
+++ main.py
@@ -1,45 +1,56 @@
-# coding=utf-8
-
+#coding=utf-8
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import *
+import queue
+import logics
 
-cellSize = 32
+cmds=queue.Queue()
+cellSize=32
+paused=False
 
-tk = Tk()
+tk=Tk()
 tk.title('Digging Treasure')
-tk.resizable(False, False)
+tk.resizable(False,False)
 
-f = Frame(tk)
-f.grid(row=0, column=0, sticky='we')
-f.columnconfigure(2, weight=1)
+def cmd(value):
+    cmds.put(value)
 
-healthBar = Progressbar(f, orient=HORIZONTAL, length=100, value=100, maximum=100, mode='determinate')
-healthBar.grid(row=0, column=0)
-Label(f, text='Life').grid(row=0, column=1)
-Label(f).grid(row=0, column=2, sticky='we')
-Label(f, text='Money').grid(row=0, column=3)
-moneyBar = Progressbar(f, orient=HORIZONTAL, length=80, value=0, maximum=1, mode='determinate')
-moneyBar.grid(row=0, column=4)
+def pause():
+    global paused
+    paused=not paused
+    pausebtn.state(['pressed' if paused else '!pressed'])
+    pausebtn['text']='Resume' if paused else 'Pause'
 
-canvas = Canvas(tk, width=10 * cellSize, height=10 * cellSize, bg='#770055')
-canvas.grid(row=1, column=0, sticky='nswe')
+f=Frame(tk)
+f.grid(row=0,column=0,sticky='we')
+f.columnconfigure(2,weight=1)
 
-infof = Frame(tk)
-infof.grid(row=2, column=0, sticky='we')
-infof.columnconfigure(1, weight=1)
+hpbar=Progressbar(f,orient=HORIZONTAL,length=100,value=100,maximum=100,mode='determinate')
+hpbar.grid(row=0,column=0)
+Label(f,text='Life').grid(row=0,column=1)
+Label(f).grid(row=0,column=2,sticky='we')
+Label(f,text='Money').grid(row=0,column=3)
+moneybar=Progressbar(f,orient=HORIZONTAL,length=80,value=0,maximum=1,mode='determinate')
+moneybar.grid(row=0,column=4)
 
-pausebtn = Button(infof, text='Pause', width=8)
-pausebtn.grid(row=0, column=0)
+canvas=Canvas(tk,width=10*cellSize,height=10*cellSize,bg='#770055')
+canvas.grid(row=1,column=0,sticky='nswe')
 
-Label(infof).grid(row=0, column=1, sticky='we')
-Button(infof, text='Instructions', width=8, command=lambda: messagebox.showinfo('Instructions', 'instructions')).grid(
-    row=0, column=2)
-Button(infof, text='About', width=8, command=lambda: messagebox.showinfo('About', 'about')).grid(row=0, column=3)
+infof=Frame(tk)
+infof.grid(row=2,column=0,sticky='we')
+infof.columnconfigure(1,weight=1)
 
-tk.bind_all('<Left>', lambda *_: cmd(lib.Command.left))
-tk.bind_all('<Right>', lambda *_: cmd(lib.Command.right))
-tk.bind_all('<Up>', lambda *_: cmd(lib.Command.next))
-tk.bind_all('<Down>', lambda *_: cmd(lib.Command.down))
+pausebtn=Button(infof,text='Pause',width=8,command=pause)
+pausebtn.grid(row=0,column=0)
+
+Label(infof).grid(row=0,column=1,sticky='we')
+Button(infof,text='Instructions',width=8,command=lambda:messagebox.showinfo('Instructions','instructions')).grid(row=0,column=2)
+Button(infof,text='About',width=8,command=lambda:messagebox.showinfo('About','about')).grid(row=0,column=3)
+
+tk.bind_all('<Left>',lambda *_:cmd(logics.Command.left))
+tk.bind_all('<Right>',lambda *_:cmd(logics.Command.right))
+tk.bind_all('<Up>',lambda *_:cmd(logics.Command.next))
+tk.bind_all('<Down>',lambda *_:cmd(logics.Command.down))
 
 mainloop()
